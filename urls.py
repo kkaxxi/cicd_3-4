f718c27108b0784aa9d4cc8a98b95bc4e189affa
Iliya Slavin <slaviniliya@gmail.com>
2024-05-29 14:18:50 +0300

    adding templates and other

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from about.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', include('about.urls')),
     path('contact/', include('contact.urls')),
     path('menu/', include('menu.urls')),
-    path('', include('about.urls')),  # Включення about як головної сторінки
+    path('', index, name='index'),  # Включення about як головної сторінки
 ]
